e2d9c1975f9a2682106c68d3d4fef426978be521
SemenchukP <semenchuk.p79@gmail.com>
2022-07-29 16:13:30 +0500

    commit1

---
+++ src/main/java/StatsService.java
@@ -0,0 +1,36 @@
+public class StatsService {
+
+    public long sum(long[] sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / 12;
+    }
+
+    public int countBeLowAvg(long[] sales) {
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+}
